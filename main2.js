b6e86d479323f617109655427300d54db44a4920
anhduy7723 <81921045+anhduy7723@users.noreply.github.com>
2023-10-27 18:01:05 +0700

    Update main2.js

--- main2.js
+++ main2.js
@@ -4,7 +4,7 @@ const categoryColors = {
   "bazo":"#66FF25",
   "oxit": "#00FFFF", 
   "axit": "#FF5733", 
-  "salt":"#A9A9A9"
+  "salt":"#00008B"
   // Add more categories and colors as needed
 };
 
